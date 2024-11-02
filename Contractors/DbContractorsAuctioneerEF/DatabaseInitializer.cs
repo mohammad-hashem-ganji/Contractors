0959b3ceeaaa6fb03cdc9a59f0c76245a875d729
Alireza Amininasab <mranotmillion@gmail.com>
2024-11-02 10:08:00 +0330

    add auto migrate

---
+++ Contractors/DbContractorsAuctioneerEF/DatabaseInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contractors.DbContractorsAuctioneerEF
+{
+    public class DatabaseInitializer
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public DatabaseInitializer(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public async Task InitializeAsync()
+        {
+            using var serviceScope = _scopeFactory.CreateScope();
+            var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            await context.Database.MigrateAsync();
+        }
+    }
+
+}
